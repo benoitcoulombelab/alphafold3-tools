f9d86bdd68620ce7482040560c8533150f28ccef
Christian Poitras <christian.poitras@ircm.qc.ca>
2026-01-14 10:10:15 -0500

    Moves loading of modules from `alphafold3-init.sh` to nextflow scripts.

--- alphafold3-init.sh
+++ alphafold3-init.sh
@@ -2,13 +2,5 @@
 
 script_path=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 
-if [[ -n "$CC_CLUSTER" ]]
-then
-  module purge
-  module load StdEnv/2023
-  module load nextflow/25.04.6
-fi
-export NXF_OPTS="-Xms500M -Xmx8000M"
-
 export PATH="$script_path":$PATH
 source "${script_path}/pairs-env/bin/activate"
--- nextflow-inference.sh
+++ nextflow-inference.sh
@@ -13,8 +13,16 @@ if ! [[ -f "${script_path}/nextflow-inference.sh" ]] && [[ -n "$SLURM_JOB_ID" ]]
 then
   script_path=$(dirname "$(scontrol show job "$SLURM_JOB_ID" | awk -F '=' '$0 ~ /Command=/ {print $2; exit}')")
 fi
-
 export SCRIPT_PATH="$script_path"
+
+# load required modules
+if [[ -n "$CC_CLUSTER" ]]
+then
+  module purge
+  module load StdEnv/2023
+  module load nextflow/25.04.6
+fi
+export NXF_OPTS="-Xms500M -Xmx8000M"
 
 echo "Launching nextflow pipeline ${script_path}/alphafold3_inference.nf"
 nextflow run ${script_path}/alphafold3_inference.nf -c "${script_path}/alphafold3.config" \
