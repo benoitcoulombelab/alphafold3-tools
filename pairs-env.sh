f3cd3774f2e0bc5937354a9e085c62826f55febf
Christian Poitras <christian.poitras@ircm.qc.ca>
2026-01-13 20:04:08 -0500

    Removes `pairs-env` folder before installing it again.

--- pairs-env.sh
+++ pairs-env.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Exit when any command fails
+set -e
+
 # load required modules
 if [[ -n "$CC_CLUSTER" ]]
 then
@@ -8,6 +11,7 @@ then
   module load python/3.13.2
 fi
 
+rm -rf ./pairs-env
 python3 -m venv ./pairs-env
 source ./pairs-env/bin/activate
 pip install .
