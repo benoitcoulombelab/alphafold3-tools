f0d70188487c61861ba2bd1e93a4399c4be1c1a6
Christian Poitras <christian.poitras@ircm.qc.ca>
2026-01-14 10:22:56 -0500

    Don't download multiple copies of `run_alphafold.py` file in alphafold3-requirements.sh.

--- alphafold3-requirements.sh
+++ alphafold3-requirements.sh
@@ -11,7 +11,7 @@ echo "Finding AlphaFold 3 requirements"
 module purge
 module load StdEnv/2023 hmmer/3.4 rdkit/2024.03.5 python/3.12
 
-wget https://raw.githubusercontent.com/google-deepmind/alphafold3/refs/tags/v3.0.1/run_alphafold.py
+wget -O run_alphafold.py https://raw.githubusercontent.com/google-deepmind/alphafold3/refs/tags/v3.0.1/run_alphafold.py
 
 virtualenv --no-download ./alphafold3_env
 source ./alphafold3_env/bin/activate
