dead210941a5361116c4a52ae048386adabbd44d
Christian Poitras <christian.poitras@ircm.qc.ca>
2026-01-16 11:46:53 -0500

    Remove Af2complexScore.py

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setup(
     ],
     entry_points={
       "console_scripts": [
-        "af2complex-score = pairs.Af2complexScore:main",
         "af3-score = pairs.Af3Score:main",
         "consensus-interface = pairs.ConsensusInterface:main",
         "delete-fasta = pairs.DeleteFasta:main",
