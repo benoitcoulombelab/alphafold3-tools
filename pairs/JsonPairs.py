711a6401b513c6e4e2947fb2c2bb901f4c751bed
Christian Poitras <christian.poitras@ircm.qc.ca>
2026-01-16 18:19:04 -0500

    Allow additional sequence in JsonPairs.py

--- pairs/JsonPairs.py
+++ pairs/JsonPairs.py
@@ -40,6 +40,8 @@ def main(argv: list[str] = None):
                       help="FASTA file containing baits")
   parser.add_argument('-t', '--targets', type=readable_file, required=True,
                       help="FASTA file containing targets")
+  parser.add_argument('-a', '--sequence', type=readable_file,
+                      help="Additional sequence element to add in JSON format.")
   parser.add_argument('-s', '--seed', type=int, nargs="*", default=None,
                       help="Seed(s) to use for model inference.  (default: use random seed)")
   parser.add_argument('-u', '--unique', action='store_true',
@@ -53,19 +55,20 @@ def main(argv: list[str] = None):
 
   args = parser.parse_args(argv)
 
-  json_pairs(baits_file=args.baits, targets_file=args.targets, seeds=args.seed,
+  json_pairs(baits_file=args.baits, targets_file=args.targets, sequence_file=args.sequence, seeds=args.seed,
              unique=args.unique, skip_identity=args.identity,
              output=args.output)
 
 
-def json_pairs(baits_file: str, targets_file: str, seeds: list[int] = None,
-    unique: bool = False,
+def json_pairs(baits_file: str, targets_file: str, sequence_file: str = None,
+    seeds: list[int] = None, unique: bool = False,
     skip_identity: bool = False, output: str = ""):
   """
   Create JSON files, each one containing a protein pair, one protein from baits and one protein from targets file.
 
   :param baits_file: FASTA file containing baits
   :param targets_file: FASTA file containing targets
+  :param sequence_file: Additional sequence element to add in JSON format
   :param seeds: seeds to use for model inference
   :param unique: save only one JSON file per unique pair -
                  do not save POLR2B-POLR2A pair if POLR2A-POLR2B is also present
@@ -77,10 +80,18 @@ def json_pairs(baits_file: str, targets_file: str, seeds: list[int] = None,
     baits = parse_fasta(baits_file_in)
   with open(targets_file, "r") as targets_file_in:
     targets = parse_fasta(targets_file_in)
+  sequence = ""
+  sequence_id = ""
+  if sequence_file:
+    with open(sequence_file, "r") as sequence_file_in:
+      sequence = json.load(sequence_file_in)
+      sequence_id = next(iter(sequence.values()))["id"]
 
   processed_ids = set()
   for bait in baits:
     bait_id = re.sub(r"[^A-Za-z]", "", bait.split("_")[0])
+    if bait_id == sequence_id:
+      bait_id = bait_id + "B"
     bait_data = {"protein": {"id": bait_id, "sequence": str(baits[bait].seq)}}
     for target in targets:
       if skip_identity and bait == target:
@@ -89,16 +100,19 @@ def json_pairs(baits_file: str, targets_file: str, seeds: list[int] = None,
       if unique and reverse_id in processed_ids:
         continue
       target_id = re.sub(r"[^A-Za-z]", "", target.split("_")[0])
-      if target_id == bait_id:
-        target_id = target_id + "A"
+      while target_id in [bait_id, sequence_id]:
+        target_id = target_id + "T"
       target_data = {
         "protein": {"id": target_id, "sequence": str(targets[target].seq)}}
       merge_id = f"{bait}__{target}"
+      sequences = [bait_data, target_data]
+      if sequence:
+        sequences.append(sequence)
       json_data = {"name": merge_id,
                    "modelSeeds": seeds if seeds else [random.randint(1,
                                                                      2147483647)],
                    "dialect": "alphafold3", "version": 1,
-                   "sequences": [bait_data, target_data]}
+                   "sequences": sequences}
       with open(os.path.join(output, f"{merge_id}.json"), 'w') as output_file:
         output_file.write(json.dumps(json_data, indent=4))
       processed_ids.add(merge_id)
--- tests/pairs/test_JsonPairs.py
+++ tests/pairs/test_JsonPairs.py
@@ -28,6 +28,7 @@ def test_main(testdir, mock_testclass):
   JsonPairs.json_pairs = MagicMock()
   JsonPairs.main(["-b", baits, "-t", targets])
   JsonPairs.json_pairs.assert_called_once_with(baits_file=ANY, targets_file=ANY,
+                                               sequence_file=None,
                                                seeds=None,
                                                unique=False,
                                                skip_identity=False,
@@ -41,15 +42,18 @@ def test_main_parameters(testdir, mock_testclass):
   open(baits, 'w').close()
   targets = "targets.fasta"
   open(targets, 'w').close()
+  sequence = "additional_sequence.json"
+  open(sequence, 'w').close()
   seed1 = 12
   seed2 = 30
   output = "output_dir"
   os.mkdir(output)
   JsonPairs.json_pairs = MagicMock()
   JsonPairs.main(
-      ["-b", baits, "-t", targets, "-s", str(seed1), str(seed2), "-u", "-i",
+      ["-b", baits, "-t", targets, "-a", sequence, "-s", str(seed1), str(seed2), "-u", "-i",
        "-o", output])
   JsonPairs.json_pairs.assert_called_once_with(baits_file=ANY, targets_file=ANY,
+                                               sequence_file=sequence,
                                                seeds=[seed1, seed2],
                                                unique=True,
                                                skip_identity=True,
@@ -63,6 +67,8 @@ def test_main_long_parameters(testdir, mock_testclass):
   open(baits, 'w').close()
   targets = "targets.fasta"
   open(targets, 'w').close()
+  sequence = "additional_sequence.json"
+  open(sequence, 'w').close()
   seed1 = 12
   seed2 = 30
   output = "output_dir"
@@ -70,9 +76,10 @@ def test_main_long_parameters(testdir, mock_testclass):
   sizes = "output_dir/sizes.txt"
   JsonPairs.json_pairs = MagicMock()
   JsonPairs.main(
-      ["--baits", baits, "--targets", targets, "--seed", str(seed1), str(seed2),
+      ["--baits", baits, "--targets", targets, "--sequence", sequence, "--seed", str(seed1), str(seed2),
        "--unique", "--identity", "--output", output])
   JsonPairs.json_pairs.assert_called_once_with(baits_file=ANY, targets_file=ANY,
+                                               sequence_file=sequence,
                                                seeds=[seed1, seed2],
                                                unique=True,
                                                skip_identity=True,
@@ -172,7 +179,7 @@ def test_json_pairs(testdir, mock_testclass):
     assert json_data["sequences"][0]["protein"]["sequence"] == polr2i.seq
     assert "protein" in json_data["sequences"][1]
     assert "id" in json_data["sequences"][1]["protein"]
-    assert json_data["sequences"][1]["protein"]["id"] == "RPBA"
+    assert json_data["sequences"][1]["protein"]["id"] == "RPBT"
     assert "sequence" in json_data["sequences"][1]["protein"]
     assert json_data["sequences"][1]["protein"]["sequence"] == polr2d.seq
   assert os.path.isfile("RPB9_HUMAN__RPB7_HUMAN.json")
@@ -196,9 +203,150 @@ def test_json_pairs(testdir, mock_testclass):
     assert json_data["sequences"][0]["protein"]["sequence"] == polr2i.seq
     assert "protein" in json_data["sequences"][1]
     assert "id" in json_data["sequences"][1]["protein"]
-    assert json_data["sequences"][1]["protein"]["id"] == "RPBA"
+    assert json_data["sequences"][1]["protein"]["id"] == "RPBT"
     assert "sequence" in json_data["sequences"][1]["protein"]
     assert json_data["sequences"][1]["protein"]["sequence"] == polr2g.seq
+
+
+def test_json_pairs_sequence(testdir, mock_testclass):
+  baits = "baits.fasta"
+  targets = "targets.fasta"
+  polr2e_file = Path(__file__).parent.joinpath("P19388.fasta")
+  polr2e = next(SeqIO.parse(polr2e_file, "fasta"))
+  polr2i_file = Path(__file__).parent.joinpath("P36954.fasta")
+  polr2i = next(SeqIO.parse(polr2i_file, "fasta"))
+  polr2d_file = Path(__file__).parent.joinpath("O15514.fasta")
+  polr2d = next(SeqIO.parse(polr2d_file, "fasta"))
+  polr2g_file = Path(__file__).parent.joinpath("P62487.fasta")
+  polr2g = next(SeqIO.parse(polr2g_file, "fasta"))
+  with open(baits, "wb") as output:
+    with open(polr2e_file, "rb") as infile:
+      shutil.copyfileobj(infile, output)
+    with open(polr2i_file, "rb") as infile:
+      shutil.copyfileobj(infile, output)
+  with open(targets, "wb") as output:
+    with open(polr2d_file, "rb") as infile:
+      shutil.copyfileobj(infile, output)
+    with open(polr2g_file, "rb") as infile:
+      shutil.copyfileobj(infile, output)
+  sequence = str(Path(__file__).parent.joinpath("atp.json"))
+  JsonPairs.json_pairs(baits_file=baits, targets_file=targets, sequence_file=sequence)
+  assert os.path.isfile("RPAB1_HUMAN__RPB4_HUMAN.json")
+  with open("RPAB1_HUMAN__RPB4_HUMAN.json", 'r') as json_file:
+    json_data = json.load(json_file)
+    assert "name" in json_data
+    assert json_data["name"] == "RPAB1_HUMAN__RPB4_HUMAN"
+    assert "modelSeeds" in json_data
+    assert len(json_data["modelSeeds"]) == 1
+    assert re.match(r"^\d+$", str(json_data["modelSeeds"][0]))
+    assert "dialect" in json_data
+    assert json_data["dialect"] == "alphafold3"
+    assert "version" in json_data
+    assert json_data["version"] == 1
+    assert "sequences" in json_data
+    assert len(json_data["sequences"]) == 3
+    assert "protein" in json_data["sequences"][0]
+    assert "id" in json_data["sequences"][0]["protein"]
+    assert json_data["sequences"][0]["protein"]["id"] == "RPAB"
+    assert "sequence" in json_data["sequences"][0]["protein"]
+    assert json_data["sequences"][0]["protein"]["sequence"] == polr2e.seq
+    assert "protein" in json_data["sequences"][1]
+    assert "id" in json_data["sequences"][1]["protein"]
+    assert json_data["sequences"][1]["protein"]["id"] == "RPB"
+    assert "sequence" in json_data["sequences"][1]["protein"]
+    assert json_data["sequences"][1]["protein"]["sequence"] == polr2d.seq
+    assert "ligand" in json_data["sequences"][2]
+    assert "id" in json_data["sequences"][2]["ligand"]
+    assert json_data["sequences"][2]["ligand"]["id"] == "AP"
+    assert "ccdCodes" in json_data["sequences"][2]["ligand"]
+    assert json_data["sequences"][2]["ligand"]["ccdCodes"][0] == "ATP"
+  assert os.path.isfile("RPAB1_HUMAN__RPB7_HUMAN.json")
+  with open("RPAB1_HUMAN__RPB7_HUMAN.json", 'r') as json_file:
+    json_data = json.load(json_file)
+    assert "name" in json_data
+    assert json_data["name"] == "RPAB1_HUMAN__RPB7_HUMAN"
+    assert "modelSeeds" in json_data
+    assert len(json_data["modelSeeds"]) == 1
+    assert re.match(r"^\d+$", str(json_data["modelSeeds"][0]))
+    assert "dialect" in json_data
+    assert json_data["dialect"] == "alphafold3"
+    assert "version" in json_data
+    assert json_data["version"] == 1
+    assert "sequences" in json_data
+    assert len(json_data["sequences"]) == 3
+    assert "protein" in json_data["sequences"][0]
+    assert "id" in json_data["sequences"][0]["protein"]
+    assert json_data["sequences"][0]["protein"]["id"] == "RPAB"
+    assert "sequence" in json_data["sequences"][0]["protein"]
+    assert json_data["sequences"][0]["protein"]["sequence"] == polr2e.seq
+    assert "protein" in json_data["sequences"][1]
+    assert "id" in json_data["sequences"][1]["protein"]
+    assert json_data["sequences"][1]["protein"]["id"] == "RPB"
+    assert "sequence" in json_data["sequences"][1]["protein"]
+    assert json_data["sequences"][1]["protein"]["sequence"] == polr2g.seq
+    assert "ligand" in json_data["sequences"][2]
+    assert "id" in json_data["sequences"][2]["ligand"]
+    assert json_data["sequences"][2]["ligand"]["id"] == "AP"
+    assert "ccdCodes" in json_data["sequences"][2]["ligand"]
+    assert json_data["sequences"][2]["ligand"]["ccdCodes"][0] == "ATP"
+  assert os.path.isfile("RPB9_HUMAN__RPB4_HUMAN.json")
+  with open("RPB9_HUMAN__RPB4_HUMAN.json", 'r') as json_file:
+    json_data = json.load(json_file)
+    assert "name" in json_data
+    assert json_data["name"] == "RPB9_HUMAN__RPB4_HUMAN"
+    assert "modelSeeds" in json_data
+    assert len(json_data["modelSeeds"]) == 1
+    assert re.match(r"^\d+$", str(json_data["modelSeeds"][0]))
+    assert "dialect" in json_data
+    assert json_data["dialect"] == "alphafold3"
+    assert "version" in json_data
+    assert json_data["version"] == 1
+    assert "sequences" in json_data
+    assert len(json_data["sequences"]) == 3
+    assert "protein" in json_data["sequences"][0]
+    assert "id" in json_data["sequences"][0]["protein"]
+    assert json_data["sequences"][0]["protein"]["id"] == "RPB"
+    assert "sequence" in json_data["sequences"][0]["protein"]
+    assert json_data["sequences"][0]["protein"]["sequence"] == polr2i.seq
+    assert "protein" in json_data["sequences"][1]
+    assert "id" in json_data["sequences"][1]["protein"]
+    assert json_data["sequences"][1]["protein"]["id"] == "RPBT"
+    assert "sequence" in json_data["sequences"][1]["protein"]
+    assert json_data["sequences"][1]["protein"]["sequence"] == polr2d.seq
+    assert "ligand" in json_data["sequences"][2]
+    assert "id" in json_data["sequences"][2]["ligand"]
+    assert json_data["sequences"][2]["ligand"]["id"] == "AP"
+    assert "ccdCodes" in json_data["sequences"][2]["ligand"]
+    assert json_data["sequences"][2]["ligand"]["ccdCodes"][0] == "ATP"
+  assert os.path.isfile("RPB9_HUMAN__RPB7_HUMAN.json")
+  with open("RPB9_HUMAN__RPB7_HUMAN.json", 'r') as json_file:
+    json_data = json.load(json_file)
+    assert "name" in json_data
+    assert json_data["name"] == "RPB9_HUMAN__RPB7_HUMAN"
+    assert "modelSeeds" in json_data
+    assert len(json_data["modelSeeds"]) == 1
+    assert re.match(r"^\d+$", str(json_data["modelSeeds"][0]))
+    assert "dialect" in json_data
+    assert json_data["dialect"] == "alphafold3"
+    assert "version" in json_data
+    assert json_data["version"] == 1
+    assert "sequences" in json_data
+    assert len(json_data["sequences"]) == 3
+    assert "protein" in json_data["sequences"][0]
+    assert "id" in json_data["sequences"][0]["protein"]
+    assert json_data["sequences"][0]["protein"]["id"] == "RPB"
+    assert "sequence" in json_data["sequences"][0]["protein"]
+    assert json_data["sequences"][0]["protein"]["sequence"] == polr2i.seq
+    assert "protein" in json_data["sequences"][1]
+    assert "id" in json_data["sequences"][1]["protein"]
+    assert json_data["sequences"][1]["protein"]["id"] == "RPBT"
+    assert "sequence" in json_data["sequences"][1]["protein"]
+    assert json_data["sequences"][1]["protein"]["sequence"] == polr2g.seq
+    assert "ligand" in json_data["sequences"][2]
+    assert "id" in json_data["sequences"][2]["ligand"]
+    assert json_data["sequences"][2]["ligand"]["id"] == "AP"
+    assert "ccdCodes" in json_data["sequences"][2]["ligand"]
+    assert json_data["sequences"][2]["ligand"]["ccdCodes"][0] == "ATP"
 
 
 def test_json_pairs_seed(testdir, mock_testclass):
@@ -293,7 +441,7 @@ def test_json_pairs_seed(testdir, mock_testclass):
     assert json_data["sequences"][0]["protein"]["sequence"] == polr2i.seq
     assert "protein" in json_data["sequences"][1]
     assert "id" in json_data["sequences"][1]["protein"]
-    assert json_data["sequences"][1]["protein"]["id"] == "RPBA"
+    assert json_data["sequences"][1]["protein"]["id"] == "RPBT"
     assert "sequence" in json_data["sequences"][1]["protein"]
     assert json_data["sequences"][1]["protein"]["sequence"] == polr2d.seq
   assert os.path.isfile("RPB9_HUMAN__RPB7_HUMAN.json")
@@ -317,7 +465,7 @@ def test_json_pairs_seed(testdir, mock_testclass):
     assert json_data["sequences"][0]["protein"]["sequence"] == polr2i.seq
     assert "protein" in json_data["sequences"][1]
     assert "id" in json_data["sequences"][1]["protein"]
-    assert json_data["sequences"][1]["protein"]["id"] == "RPBA"
+    assert json_data["sequences"][1]["protein"]["id"] == "RPBT"
     assert "sequence" in json_data["sequences"][1]["protein"]
     assert json_data["sequences"][1]["protein"]["sequence"] == polr2g.seq
 
@@ -418,7 +566,7 @@ def test_json_pairs_seeds(testdir, mock_testclass):
     assert json_data["sequences"][0]["protein"]["sequence"] == polr2i.seq
     assert "protein" in json_data["sequences"][1]
     assert "id" in json_data["sequences"][1]["protein"]
-    assert json_data["sequences"][1]["protein"]["id"] == "RPBA"
+    assert json_data["sequences"][1]["protein"]["id"] == "RPBT"
     assert "sequence" in json_data["sequences"][1]["protein"]
     assert json_data["sequences"][1]["protein"]["sequence"] == polr2d.seq
   assert os.path.isfile("RPB9_HUMAN__RPB7_HUMAN.json")
@@ -443,7 +591,7 @@ def test_json_pairs_seeds(testdir, mock_testclass):
     assert json_data["sequences"][0]["protein"]["sequence"] == polr2i.seq
     assert "protein" in json_data["sequences"][1]
     assert "id" in json_data["sequences"][1]["protein"]
-    assert json_data["sequences"][1]["protein"]["id"] == "RPBA"
+    assert json_data["sequences"][1]["protein"]["id"] == "RPBT"
     assert "sequence" in json_data["sequences"][1]["protein"]
     assert json_data["sequences"][1]["protein"]["sequence"] == polr2g.seq
 
@@ -511,7 +659,7 @@ def test_json_pairs_unique(testdir, mock_testclass):
     assert json_data["sequences"][0]["protein"]["sequence"] == polr2e.seq
     assert "protein" in json_data["sequences"][1]
     assert "id" in json_data["sequences"][1]["protein"]
-    assert json_data["sequences"][1]["protein"]["id"] == "RPABA"
+    assert json_data["sequences"][1]["protein"]["id"] == "RPABT"
     assert "sequence" in json_data["sequences"][1]["protein"]
     assert json_data["sequences"][1]["protein"]["sequence"] == polr2e.seq
   assert not os.path.isfile("RPB9_HUMAN__RPAB1_HUMAN.json")
@@ -536,7 +684,7 @@ def test_json_pairs_unique(testdir, mock_testclass):
     assert json_data["sequences"][0]["protein"]["sequence"] == polr2i.seq
     assert "protein" in json_data["sequences"][1]
     assert "id" in json_data["sequences"][1]["protein"]
-    assert json_data["sequences"][1]["protein"]["id"] == "RPBA"
+    assert json_data["sequences"][1]["protein"]["id"] == "RPBT"
     assert "sequence" in json_data["sequences"][1]["protein"]
     assert json_data["sequences"][1]["protein"]["sequence"] == polr2i.seq
 
@@ -755,7 +903,7 @@ def test_json_pairs_output(testdir, mock_testclass):
     assert json_data["sequences"][0]["protein"]["sequence"] == polr2i.seq
     assert "protein" in json_data["sequences"][1]
     assert "id" in json_data["sequences"][1]["protein"]
-    assert json_data["sequences"][1]["protein"]["id"] == "RPBA"
+    assert json_data["sequences"][1]["protein"]["id"] == "RPBT"
     assert "sequence" in json_data["sequences"][1]["protein"]
     assert json_data["sequences"][1]["protein"]["sequence"] == polr2d.seq
   assert os.path.isfile(os.path.join(output, "RPB9_HUMAN__RPB7_HUMAN.json"))
@@ -780,6 +928,6 @@ def test_json_pairs_output(testdir, mock_testclass):
     assert json_data["sequences"][0]["protein"]["sequence"] == polr2i.seq
     assert "protein" in json_data["sequences"][1]
     assert "id" in json_data["sequences"][1]["protein"]
-    assert json_data["sequences"][1]["protein"]["id"] == "RPBA"
+    assert json_data["sequences"][1]["protein"]["id"] == "RPBT"
     assert "sequence" in json_data["sequences"][1]["protein"]
     assert json_data["sequences"][1]["protein"]["sequence"] == polr2g.seq
