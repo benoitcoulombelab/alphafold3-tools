bad6c427e82f2554870763f2494788281c6390c2
Christian Poitras <christian.poitras@ircm.qc.ca>
2026-05-07 13:41:31 -0400

    Add script to create pip wheel for AlphaFold 3

---
+++ create-alphafold3-wheel.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+# Exit when any command fails
+set -e
+
+script_name=$(basename "${BASH_SOURCE[0]}")
+current_directory=$(pwd)
+
+# Default argument values.
+version=v3.0.2
+development=
+requirements=alphafold3-requirements.txt
+
+# Usage function
+usage() {
+  echo
+  echo "Creates AlphaFold 3 python wheel"
+  echo
+  echo "Usage: $script_name [-v <version>] [-d] [-r <requirements>] [-h]"
+  echo "  -v: Version (tag or commit hash) of AlphaFold 3 to create wheel for (default: v3.0.2)"
+  echo "  -d: Version is a development, '-dev' will be appended to the version (default: '-dev' not appended)"
+  echo "  -r: PIP requirements output (default: alphafold3-requirements.txt)"
+  echo "  -h: Show this help and squire Map help"
+  echo
+  echo "This script needs to be adapted to work on specific versions and commits and will work only"
+  echo "on Alliance Canada clusters for commits around version v3.0.2"
+  echo
+}
+
+# Parsing arguments.
+while getopts 'v:dr:h' OPTION; do
+  case "$OPTION" in
+    v)
+       version="$OPTARG"
+       ;;
+    d)
+       development=true
+       ;;
+    r)
+       requirements="$OPTARG"
+       ;;
+    h)
+       usage
+       exit 0
+       ;;
+    :)
+       usage
+       exit 1
+       ;;
+    ?)
+       usage
+       exit 1
+       ;;
+  esac
+done
+
+
+module purge
+module load StdEnv/2023
+module load python/3.13 rdkit/2025.09.4 cmake
+
+tmp_dir=$(mktemp -d "${TMPDIR:-/tmp}/$(basename "$0").XXXXXXXXXXXX")
+trap 'rm -rf "$tmp_dir"; exit' ERR EXIT
+
+echo "Creating python virtualenv"
+virtualenv --clear "${tmp_dir}/AF3-ENV" && source "${tmp_dir}/AF3-ENV/bin/activate"
+pip install -U pip
+
+echo "Cloning AlphaFold 3 version $version"
+git clone https://github.com/google-deepmind/alphafold3.git "${tmp_dir}/alphafold3"
+cd "${tmp_dir}/alphafold3"
+git checkout "$version"
+if [[ -n "$development" ]]
+then
+  sed -i 's/\(version = "[0-9.]*\)"/\1-dev"/' pyproject.toml
+fi
+
+echo "Build AlphaFold 3 wheel"
+CMAKE_BUILD_PARALLEL_LEVEL=8 pip wheel --no-deps --no-index -v .
+wheels=(alphafold3*.whl)
+if [[ -f "${wheels[0]}" ]]
+then
+  wheel=${wheels[0]}
+else
+  >&2 echo "Error: failed to create wheel for AlphaFold 3, exiting..."
+  exit 1
+fi
+
+echo "Creating pip requirements file"
+cp "$wheel" "$current_directory"
+pip install --no-index "$current_directory/$wheel"
+pip freeze --local > "${tmp_dir}/${requirements}"
+
+deactivate
+
+cp "${tmp_dir}/${requirements}" "$current_directory"
+
+echo
+echo "Copied AlphaFold 3 wheel to $current_directory/$wheel and pip requirements to $current_directory/$requirements"
